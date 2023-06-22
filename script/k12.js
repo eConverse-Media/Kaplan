bb88a5cd730fe4dcbf9e812ff16a1547cfe83171
eCM-MDykeman <64661174+eCM-MDykeman@users.noreply.github.com>
2023-06-22 15:55:45 -0400

    Adding script and styles

---
+++ script/k12.js
@@ -0,0 +1,4 @@
+var k12Header = customHtmlRoot.shadowRoot.querySelector('.k-12 .heading');
+var themeHeader = document.querySelector("#themeHeader").shadowRoot;
+var pencil = themeHeader.querySelector('#pencil-icon');
+k12.appendChild(pencil);
